4451452ec12effa74ec4a6c5e5f9196f8387a0ed
GaetanoRe <gaetanore@live.com>
2025-09-06 21:40:20 -0400

    made hazard hitboxes

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -25,6 +25,7 @@ var hurtbox_col : CollisionShape2D
 var direction_point : Vector2
 var direction_str : String = "down"
 var animationflip : bool = false
+var current_anim : String = ""
 
 var sprite : AnimatedSprite2D
 
@@ -40,6 +41,7 @@ func _ready() -> void:
 	hurtbox_col = get_node("Hurtbox/DamageCollision")
 	sprite = get_node("AnimatedSprite2D")
 	sprite.frame_changed.connect(_on_frame_changed)
+
 
 func _physics_process(delta: float) -> void:
 	if is_rolling:
@@ -57,7 +59,6 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity = Vector2.ZERO
 
-		# Start roll
 		# Start roll
 	if Input.is_action_just_pressed("dodge"):
 		# Use current input if available, otherwise fallback to last facing
@@ -95,33 +96,32 @@ func adjust_item_anchor() -> void:
 		item_anchor.global_rotation = deg_to_rad(-90)
 
 func adjust_animations() -> void:
-	var animation_dir : String
-	# decide left/right flips from direction_str
-	if direction_str == "left":
-		animationflip = true
-	else:
-		animationflip = false
+	var animation_dir : String = "side" if (direction_str == "left" or direction_str == "right") else direction_str
+	animationflip = (direction_str == "left")
 
-	# map to "side"/"up"/"down" groups
-	if direction_str == "left" or direction_str == "right":
-		animation_dir = "side"
-	else:
-		animation_dir = direction_str
-
-	# *** PRIORITY: roll -> walk -> idle ***
+	var target_anim := ""
 	if is_rolling:
-		sprite.play("dodge_" + animation_dir)
-		sprite.flip_h = animationflip
+		target_anim = "dodge_" + animation_dir
 	elif direction_point != Vector2.ZERO:
-		sprite.play("walk_" + animation_dir)
-		sprite.flip_h = animationflip
+		target_anim = "walk_" + animation_dir
 	else:
-		sprite.play("idle_" + animation_dir)
-		sprite.flip_h = animationflip
+		target_anim = "idle_" + animation_dir
+
+	if sprite.animation != target_anim or !sprite.is_playing():
+		sprite.play(target_anim)
+	current_anim = target_anim
+	sprite.flip_h = animationflip
+
 
 func _on_frame_changed():
-	if sprite.animation.begins_with("dodge"):
-		if sprite.frame == 0:
-			hurtbox_col.disabled = true
-		elif sprite.frame == sprite.sprite_frames.get_frame_count(sprite.animation) - 1:
-			hurtbox_col.disabled = false
+	if sprite.animation.begins_with("dodge") and sprite.frame == 0:
+		print("Animation Started")
+		hurtbox_col.disabled = true
+	elif sprite.animation.begins_with("dodge") and sprite.frame == 6:
+		print("Animation ended!!!") 
+		hurtbox_col.disabled = false
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if(area.is_in_group("Hazard")):
+		player_heath -= 25
+		print("Hazard Entered Hurtbox")
